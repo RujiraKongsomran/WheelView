72c3dc4c8a5fae41a945ab0454942a87dd59bea6
RujiraKongsomran <rujirabiwberry@gmail.com>
2020-07-21 22:31:14 +0700

    Write JAVA code

--- app/src/main/java/com/rujirakongsomran/wheelview/MainActivity.java
+++ app/src/main/java/com/rujirakongsomran/wheelview/MainActivity.java
@@ -3,6 +3,14 @@ package com.rujirakongsomran.wheelview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import kotlin.Unit;
+import kotlin.jvm.functions.Function1;
+import me.sujanpoudel.wheelview.WheelView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final WheelView wheelView = (WheelView) findViewById(R.id.wheel_view);
+        wheelView.setTitles(Arrays.asList("First", "Second", "Third", "Fourth", "Fifth", "Sixth"));
+        wheelView.setSelectListener(new Function1<Integer, Unit>() {
+            @Override
+            public Unit invoke(Integer integer) {
+                showToast(integer);
+                return Unit.INSTANCE;
+            }
+
+            private final void showToast(Integer integer) {
+                Toast.makeText(MainActivity.this, "" + wheelView.getTitles().get(integer), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+
 }
